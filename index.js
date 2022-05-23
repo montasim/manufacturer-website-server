732bf4ac184c430671c288fa4bab78ed240932cc
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-23 12:58:48 +0600

    single product

--- index.js
+++ index.js
@@ -49,6 +49,15 @@ async function run() {
 
             res.send(products);
         });
+
+        // single product
+        app.get('/products/:id', async (req, res) => {
+            const id = req?.params?.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+
+            res.send(product);
+        });
     }
     finally {
 
