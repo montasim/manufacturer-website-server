2443b9d4cd4644f5f695c5c1911bd698134d3409
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-05-23 10:54:40 +0600

    invalid server path will display error

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const jsonwebtoken = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const app = express();
+const port = process.env.PORT || 5000;
+const serverName = process.env.DB_SERVER_NAME;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fkzlo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = _ => {
+    try {
+
+    }
+    finally {
+
+    }
+};
+
+app.get('/', (req, res) => {
+    res.send(`Welcome to ${serverName} server`);
+});
+
+app.get('/index', (req, res) => {
+    res.send(`Welcome to ${serverName} server`);
+});
+
+app.get('/home', (req, res) => {
+    res.send(`Welcome to ${serverName} server`);
+});
+
+app.get('*', (req, res) => {
+    res.send(`Try with a valid server URL`);
+});
+
+app.listen(port, () => {
+    console.log(`${serverName} is running on port ${port}`);
+});
 
